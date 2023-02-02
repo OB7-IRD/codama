1d8aca7ad88d9dcc546cf8f0597795479ab9a518
OceaneBouhineau <oceane.bouhineau@ird.fr>
2023-02-02 10:40:14 +0100

    add function check_specie_catch_ocean
    - Change SQL queries : specie_catch_ocean.sql ; specie_ocean.sql
    - Add function check_specie_catch_ocean

--- R/codama-package.R
+++ R/codama-package.R
@@ -1,4 +1,3 @@
 #'@title Common prOcesses for Data quAlity Monitoring
-#'@description This package aims is to provide a series of standardised and generic controls processes for data quality assessment and monitoring.
 #' @keywords internal
 "_PACKAGE"
--- inst/sql/specie_catch_ocean.sql
+++ inst/sql/specie_catch_ocean.sql
@@ -1,11 +1,11 @@
 --- Recovers information about the species caught, the ocean declared in the trip
 SELECT 
-    c.topiaid::text AS topiaid_catch, 
-    o.label1::text AS ocean, 
-    s.label1::text AS species, 
-    s.topiaid::text AS topiaid_species,
-    so.ocean::text AS ocean_species_ocean, 
-    so.species::text AS species_species_ocean
+    c.topiaid::text AS catch_id, 
+    o.topiaid::text AS ocean_id, 
+    s.topiaid::text AS specie_id,
+    s.label1::text AS specie_name, 
+    o.label1::text AS ocean_name
+
 FROM 
     ps_logbook.catch c 
     INNER JOIN common.species s ON c.species = s.topiaid 
@@ -13,6 +13,6 @@ FROM
     INNER JOIN ps_logbook.route r ON a.route = r.topiaid 
     INNER JOIN ps_common.trip t ON r.trip = t.topiaid 
     INNER JOIN common.ocean o ON t.ocean = o.topiaid 
-    LEFT JOIN common.species_ocean so ON so.ocean= o.topiaid AND so.species = s.topiaid 
 WHERE 
-    t.topiaid IN (?trip)
+    t.topiaid IN (?select_item) OR
+    EXTRACT(year FROM t.startdate) IN (?select_item) OR EXTRACT(year FROM t.enddate) IN (?select_item)
