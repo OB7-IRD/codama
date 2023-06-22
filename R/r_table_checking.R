911079d6995fd7d1d692984a8df0fe03d247fcfc
OceaneBouhineau <oceane.bouhineau@ird.fr>
2023-06-22 09:34:41 +0200

    Add r_table_checking
    - Add r_table_checking.R
    - Change r_type_checking.R : add type "data.frame" and "tbl"

---
+++ R/r_table_checking.R
@@ -0,0 +1,143 @@
+#' @name r_table_checking
+#' @title Checking for table object
+#' @description Type, name and type of mandatory columns are controlled
+#' @param r_table Table object expected.
+#' @param type Object of class \code{\link[base]{character}} expected. Type or class expected of the r table You can choose between arguments "data.frame", or "tbl".
+#' @param column_name \code{\link[base]{vector}} of {\link[base]{character}} expected. Be default NULL. Mandatory column names in r_table.
+#' @param column_type \code{\link[base]{vector}} of {\link[base]{character}} expected. Be default NULL. Mandatory column type in r_table.
+#' @param output {\link[base]{character}} expected. Kind of expected output. You can choose between "message" or "logical".
+#' @return The function returns a {\link[base]{character}} with output is "message", a {\link[base]{logical}} with output is "logical"
+#' @export
+r_table_checking <- function(r_table,
+                             type,
+                             column_name = NULL,
+                             column_type = NULL,
+                             output) {
+  # 0 - Global variables assignement ----
+  # 1 - Arguments verification ----
+  # Checks the type and values of type
+  if (r_type_checking(
+    r_object = type,
+    type = "character",
+    allowed_value = c("data.frame", "tbl"),
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = type,
+      type = "character",
+      allowed_value = c("data.frame", "tbl"),
+      output = "message"
+    ))
+  }
+  # Checks the type of column_name
+  if (!is.null(column_name) && r_type_checking(
+    r_object = column_name,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = column_name,
+      type = "character",
+      output = "message"
+    ))
+  }
+  # Checks the type of column_type
+  if (!is.null(column_type) && r_type_checking(
+    r_object = column_type,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = column_type,
+      type = "character",
+      output = "message"
+    ))
+  }
+  # Checks the type and values of output
+  if (r_type_checking(
+    r_object = output,
+    type = "character",
+    allowed_value = c("message", "logical"),
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = output,
+      type = "character",
+      allowed_value = c("message", "logical"),
+      output = "message"
+    ))
+  }
+  # Checks the length of column_name and column_type
+  if (length(x = column_name) != length(x = column_type) && !is.null(column_type)) {
+    stop(cat(
+      format(
+        x = Sys.time(),
+        "%Y-%m-%d %H:%M:%S"
+      ),
+      " Error - invalid data, different sizes between \"column_name\" and \"column_type\"\n"
+    ))
+  }
+  # 2 - global process ----
+  # Checks the type of r_table
+  if (r_type_checking(
+    r_object = r_table,
+    type = type,
+    output = "logical"
+  ) != TRUE) {
+    if (output == "message") {
+      return(r_type_checking(
+        r_object = r_table,
+        type = type,
+        output = "message"
+      ))
+    }
+    if (output == "logical") {
+      return(FALSE)
+    }
+  }
+  # Checks column names
+  checks_column_names <- column_name %in% colnames(r_table)
+  if (sum(checks_column_names) != length(column_name)) {
+    if (output == "message") {
+      stop(cat(
+        format(
+          x = Sys.time(),
+          "%Y-%m-%d %H:%M:%S"
+        ),
+        paste0(" Error - invalid data, the following column names in \"column_name\" were not found in \"r_table\" : ", paste0(column_name[!checks_column_names], collapse = ", "), "\n")
+      ))
+    }
+    if (output == "logical") {
+      return(FALSE)
+    }
+  }
+  # Checks column type
+  checks_column_type <- sapply(r_table[, column_name], class) == column_type
+  if (sum(checks_column_type) != length(column_type)) {
+    if (output == "message") {
+      stop(cat(
+        format(
+          x = Sys.time(),
+          "%Y-%m-%d %H:%M:%S"
+        ),
+        paste0("Error - invalid data, the following column names in \"column_name\" are not of the type of \"column_type\" in \"r_table\". \n", paste0(paste0("column names :", colnames(r_table[, column_name])[!checks_column_type], " ; type in \"r_table\" :", sapply(r_table[, colnames(r_table[, column_name])[!checks_column_type], drop = FALSE], class), "; type in \"column_name\" :", column_type[!checks_column_type], collapse = ",\n"), "\n", collapse = ", "))
+      ))
+    }
+    if (output == "logical") {
+      return(FALSE)
+    }
+  }
+  if (output == "message") {
+    return(cat(
+      format(
+        x = Sys.time(),
+        format = "%Y-%m-%d %H:%M:%S"
+      ),
+      " - Process \"r_table_checking\" ran successfully.\n",
+      sep = ""
+    ))
+  }
+  if (output == "logical") {
+    return(TRUE)
+  }
+}
