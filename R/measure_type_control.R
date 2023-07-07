f027fcdc2b3a25f3778fff7a8e379994be92db30
Esther MOLLIER <esther.mollier@ird.fr>
2023-07-07 11:32:20 +0200

    Final release measure_type_control function

---
+++ R/measure_type_control.R
@@ -0,0 +1,265 @@
+#' @name measure_type_control
+#' @title Measure_type_control
+#' @description This function allows to check the coherence of a sampled species measure type according to its default measure type.
+#' @param data_connection {\link[base]{list}} expected. Either output of the function {\link[furdeb]{postgresql_dbconnection}}, which must be done before using the measure_type_control
+#' @param start_year {\link[base]{integer}} expected. Starting year for the control.
+#' @param end_year {\link[base]{integer}} expected. Ending year for the control.
+#' @param program {\link[base]{character}} expected. Programs to be controlled. Example of the format for a program topiaid: "fr.ird.referential.ps.common.Program#1239832686262#0.31033946454061234"
+#' @param ocean {\link[base]{character}} expected. Ocean to be controlled. Examples: 'Indian', 'Atlantic'...etc.
+#' @param country_code {\link[base]{character}} expected. Countries on wich control will be made. Examples: 'FRA', 'MUS'...etc.
+#' @param path_file {\link[base]{character}} expected. Path to save the final xlsx.
+#' @return The function return a xlsx table.
+#' @export
+#' @importFrom DBI dbGetQuery sqlInterpolate SQL
+#' @importFrom dplyr group_by summarise select mutate
+#' @importFrom lubridate now
+#' @importFrom openxlsx write.xlsx
+measure_type_control <- function(data_connection,
+                                 start_year,
+                                 end_year,
+                                 program,
+                                 ocean,
+                                 country_code,
+                                 path_file = NULL) {
+  # 0 - Global variables assignement ----
+  fao_code <- NULL
+  size_type <- NULL
+  count <- NULL
+  default_measure_type <- NULL
+  fao_code <- NULL
+  correspondence <- NULL
+  # 1 - Arguments verification ----
+  if (r_type_checking(
+    r_object = start_year,
+    type = "integer",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = start_year,
+      type = "integer",
+      output = "message"
+    ))
+  }
+  if (r_type_checking(
+    r_object = end_year,
+    type = "integer",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = end_year,
+      type = "integer",
+      output = "message"
+    ))
+  }
+  if (r_type_checking(
+    r_object = program,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = program,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (r_type_checking(
+    r_object = ocean,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = ocean,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (r_type_checking(
+    r_object = country_code,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = country_code,
+      type = "character",
+      output = "message"
+    ))
+  }
+  if (!is.null(x = path_file) && r_type_checking(
+    r_object = path_file,
+    type = "character",
+    output = "logical"
+  ) != TRUE) {
+    return(r_type_checking(
+      r_object = path_file,
+      type = "character",
+      output = "message"
+    ))
+   }
+  # 2 - Data extraction ----
+  if (data_connection[[1]] == "observe") {
+    observe_sample_sql <- paste(
+      readLines(con = system.file("sql",
+                                  "observe_sample.sql",
+                                  package = "codama"
+      )),
+      collapse = "\n"
+    )
+    observe_species_sql <- paste(
+      readLines(con = system.file("sql",
+                                  "observe_species.sql",
+                                  package = "codama"
+      )),
+      collapse = "\n"
+    )
+    # Correction of the sql query if ocean or country code not selected
+    if ("%" %in% ocean) {
+      observe_sample_sql <- sub(
+        pattern = "AND o.label1 in (?ocean)",
+        replacement = "AND o.label1 like (?ocean)",
+        x = observe_sample_sql,
+        fixed = TRUE
+      )
+    }
+    if ("%" %in% country_code) {
+      observe_sample_sql <- sub(
+        pattern = "AND co.iso3code in (?country_code)",
+        replacement = "AND co.iso3code like (?country_code)",
+        x = observe_sample_sql,
+        fixed = TRUE
+      )
+    }
+    observe_sample_sql_final <- DBI::sqlInterpolate(
+      conn = data_connection[[2]],
+      sql = observe_sample_sql,
+      start_year = DBI::SQL(paste0(paste0(start_year, collapse = ", "))),
+      end_year = DBI::SQL(paste0(paste0(end_year, collapse = ", "))),
+      program = DBI::SQL(paste0("'", paste0(program, collapse = "', '"), "'")),
+      ocean = DBI::SQL(paste0("'", paste0(ocean, collapse = "', '"), "'")),
+      country_code = DBI::SQL(paste0("'", paste0(country_code, collapse = "', '"), "'"))
+    )
+    sample <- dplyr::tibble(DBI::dbGetQuery(
+      conn = data_connection[[2]],
+      statement = observe_sample_sql_final
+    ))
+    species <- dplyr::tibble(DBI::dbGetQuery(
+      conn = data_connection[[2]],
+      statement = observe_species_sql
+    ))
+  }
+  # 3 - Data design ----
+  ## Overall measure type control
+  ### Remove the duplicated fao code (link to the ocean column)
+  species <- species[!duplicated(species$fao_code), ]
+  ### Summarise sample by fao code and size-type
+  summarise_samplemeasure <- sample %>%
+    dplyr::group_by(fao_code, size_type) %>%
+    dplyr::summarise(nb_measure = sum(count), .groups = "drop")
+  #### Summarise species
+  summarise_species <- species %>%
+    dplyr::select(-ocean)
+  ### Merge of the two tables to add the default measure type for each species
+  summarise_samplemeasure_species <- merge(summarise_samplemeasure,
+                                           summarise_species,
+                                           by = c("fao_code"),
+                                           all.x = TRUE,
+                                           all.y = FALSE
+  )
+  ### Add a column to check if both measure types are the same
+  overall_measure_type_control <- summarise_samplemeasure_species %>%
+    dplyr::mutate(correspondence = ifelse(size_type == default_measure_type,
+                                          TRUE,
+                                          FALSE
+    ))
+  ## Detailed measure type control
+  ### Selection of the species for which we found an inconsistency
+  inconsistent_observation <- overall_measure_type_control %>%
+    dplyr::filter(correspondence == FALSE)
+  print(paste(
+    " Observation(s) with a wrong measure type: ",
+    nrow(inconsistent_observation)
+  ))
+  for (i in nrow(inconsistent_observation)) {
+    print(paste(
+      inconsistent_observation$fao_code,
+      " in ",
+      inconsistent_observation$size_type
+    ))
+  }
+  ### Found in catch data these observations
+  detailed_measure_type_control <- data.frame()
+  for (i in nrow(inconsistent_observation)) {
+    detailed_measure_type_control <- rbind(
+      detailed_measure_type_control,
+      sample %>%
+        dplyr::filter(fao_code == inconsistent_observation$fao_code[i] &
+                        size_type == inconsistent_observation$size_type[i])
+    )
+  }
+  # 4 - Export ----
+  ## Fold creation for the overall measure type control
+  folder_overall_measure_type_control <- paste0(
+    path_file,
+    "/overall_measure_type_control"
+  )
+  if (file.exists(folder_overall_measure_type_control) == FALSE) {
+    dir.create(folder_overall_measure_type_control)
+  }
+  ## Export overall measure type control
+  timestamp <- format(
+    lubridate::now(),
+    "%Y%m%d_%H%M%S"
+  )
+  if (!is.null(x = path_file)) {
+    openxlsx::write.xlsx(overall_measure_type_control,
+                         file = paste0(
+                           path_file,
+                           "/overall_measure_type_control",
+                           "/overall_measure_type_control_",
+                           country_code,
+                           "_",
+                           ocean,
+                           "_",
+                           start_year,
+                           "-",
+                           end_year,
+                           "_",
+                           timestamp,
+                           ".xlsx"
+                         ),
+                         rowNames = FALSE
+    )
+  }
+  ## Fold creation for the detailed species control
+  folder_detailed_measure_type_control <- paste0(
+    path_file,
+    "/detailed_measure_type_control"
+  )
+  if (file.exists(folder_detailed_measure_type_control) == FALSE) {
+    dir.create(folder_detailed_measure_type_control)
+  }
+  ## Export overall species control
+  timestamp <- format(
+    lubridate::now(),
+    "%Y%m%d_%H%M%S"
+  )
+  if (!is.null(x = path_file)) {
+    openxlsx::write.xlsx(detailed_measure_type_control,
+                         file = paste0(
+                           path_file,
+                           "/detailed_measure_type_control",
+                           "/detailed_measure_type_control_",
+                           country_code,
+                           "_",
+                           ocean,
+                           "_",
+                           start_year,
+                           "-",
+                           end_year,
+                           "_",
+                           timestamp,
+                           ".xlsx"
+                         ),
+                         rowNames = FALSE
+    )
+  }
+}
---
+++ inst/sql/observe_sample.sql
@@ -0,0 +1,61 @@
+SELECT
+p.label1::text as program
+,extract(year from r.date)::integer as year
+,o.label1::text as ocean
+,v.label1::text as vessel
+,t.homeid::text as home_id
+,t.startdate::date as trip_start_date
+,t.enddate::date as trip_end_date
+,ob.lastname::text as observer
+,r.date::date as observation_date
+,a.time::time as observation_time
+,a.latitude::numeric as latitude
+,a.longitude::numeric as longitude
+,(CASE WHEN st.code='0' THEN 'UNK' ELSE (CASE WHEN st.code='1' THEN 'FOB' ELSE (CASE WHEN st.code='2' THEN 'FSC' END) END) END)::text as school_type
+,sg.label1::text as species_group
+,sp.faocode::text as fao_code
+,sp.label1::text as common_name
+,sp.scientificlabel::text as scientific_name
+,sm.count::integer count
+,sm.weight::numeric as weight
+,sm.isweightcomputed::boolean as weight_was_computed
+,sp.label2::text as nom_commun
+,sm.length::numeric as length
+,smt.code::text as size_type
+,sm.islengthcomputed::boolean as length_was_computed
+,sx.label1::text as sex
+,sf.label1::text as fate
+,sf.code::integer as fate_code
+,sf.topiaid::text as fate_id
+,sa.comment::text as sample_comment
+,t.topiaid::text as trip_id
+,s.topiaid::text as set_id
+,sa.topiaid::text as sample_id
+,sm.topiaid::text as samplemeasure_id
+
+FROM ps_common.trip t
+INNER JOIN ps_common.program p ON (t.observationsprogram = p.topiaid)
+INNER JOIN common.ocean o ON (t.ocean = o.topiaid)
+INNER JOIN common.person ob ON (t.observer = ob.topiaid)
+INNER JOIN common.vessel v ON (t.vessel = v.topiaid)
+INNER JOIN common.country co ON (v.flagcountry = co.topiaid)
+INNER JOIN ps_observation.route r ON (r.trip = t.topiaid)
+INNER JOIN ps_observation.activity a ON (a.route = r.topiaid)
+INNER JOIN ps_common.vesselactivity va ON (a.vesselactivity = va.topiaid)
+INNER JOIN ps_observation.set s ON (s.activity = a.topiaid)
+INNER JOIN ps_observation.sample sa ON (sa.set = s.topiaid)
+INNER JOIN ps_observation.samplemeasure sm ON (sm.sample = sa.topiaid)
+INNER JOIN common.sizemeasuretype smt ON (sm.sizemeasuretype = smt.topiaid)
+INNER JOIN common.species sp ON (sm.species = sp.topiaid) 
+INNER JOIN common.speciesgroup sg ON (sp.speciesgroup = sg.topiaid)
+LEFT OUTER JOIN common.sex sx ON (sm.sex = sx.topiaid)
+LEFT OUTER JOIN ps_common.speciesfate sf ON (sm.speciesfate = sf.topiaid)
+LEFT OUTER JOIN ps_common.schooltype st ON (s.schooltype = st.topiaid)
+
+WHERE
+extract(year from r.date) between (?start_year) and (?end_year)
+AND p.topiaid in (?program)
+AND o.label1 in (?ocean)
+AND co.iso3code in (?country_code)
+
+;
